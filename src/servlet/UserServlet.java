31ab7b90d3f2e8f5fcfe5ab9a35486233f14bae1
iyang <ivan.yang@wundermanthompson.com>
2024-03-06 09:09:14 +0000

    Initial commit

---
+++ src/servlet/UserServlet.java
@@ -0,0 +1,26 @@
+package servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/user")
+public class UserServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");//根据参数名获取参数值，无论参数是在url?后面还是在请求体中
+        //根据用户名判断用户是否存在
+        resp.setHeader("Content-Type", "text/html");
+        resp.setContentType("text/html;charset=utf-8");
+        if("admin".equals(username)) {
+            resp.getWriter().write("true");
+        } else {
+            resp.getWriter().write("false");
+
+        }
+    }
+}
